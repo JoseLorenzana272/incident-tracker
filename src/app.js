05d64e1d8a3451d2aa2644c84bcff72dd4748421
JoseLorenzana272 <jodanielorenzana@gmail.com>
2026-04-14 16:49:24 -0600

    fix: forced solution

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const { errorHandler }    = require('./middleware/errorHandler');
 const { auditLogger }     = require('./middleware/auditLogger');
 
 // here, bad practice:
-process.env.DATABASE_URL = 'postgres://admin:SuperSecret123@db.produccion.com:5432/main';
 
 const app = express();
 
